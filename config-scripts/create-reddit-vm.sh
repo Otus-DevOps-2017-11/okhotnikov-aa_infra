8771c6a1a852a1da4e2aa30f10cb0e612fea1cc0
okhotnikov-aa <okhotnikov.a.a@gmail.com>
2018-02-14 21:16:31 +0300

    * task changes

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -3,7 +3,7 @@ gcloud compute instances create instance-from-gcloud-cli \
   --image=ubuntu-1604-xenial-v20170815a \
   --image-project=ubuntu-os-cloud \
   --machine-type=f1-micro \
-  --labels=role=willberemovedin24h \
+  --tags" \
   --preemptible \
   --restart-on-failure \
-  --zone=europe-west1-d
+  --zone=europe-west4-b
